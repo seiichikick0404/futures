dcbe5ad65e3407d459ce949d5b332d14d50119d9
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-09-22 17:59:03 +0900

    jsでプルダウンメニューの作成

---
+++ app/javascript/pulldown.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById('lists')
+  const pullDownLists = document.getElementById('pull-down')
+  
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "border: solid")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style", "border-style:none;")
+  })
+
+  pullDownButton.addEventListener('click', function(){
+    if (pullDownLists.getAttribute("style") == "display:block;") {
+      // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
+      pullDownLists.removeAttribute("style", "display:block;")
+    } else {
+      // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
+      pullDownLists.setAttribute("style", "display:block;")
+    }
+  })
+})
