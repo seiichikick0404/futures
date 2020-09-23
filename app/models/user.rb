7ec01ab9ee19217ce71c2e9020b70f8c6a439455
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-09-23 16:54:07 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, length: { minimum: 6 },
+                       format: { with: /\A(?=.*?[a-z])[a-z\d]{6,32}+\z/ }
 end
