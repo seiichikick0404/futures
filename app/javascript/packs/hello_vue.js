75439644177ee17fbd940fea85ed9f795fc2b495
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-11-06 14:58:55 +0900

    vueのコメントアウト

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -9,14 +9,14 @@
 // import Vue from 'vue'
 // import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     render: h => h(App)
+//   }).$mount()
+//   document.body.appendChild(app.$el)
 
-  console.log(app)
-})
+//   console.log(app)
+// })
 
 
 // The above code uses Vue without the compiler, which means you cannot
