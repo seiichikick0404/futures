024187fe2f6b25de71bf553da77e10f27d68cf06
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-12-12 08:38:40 +0900

    ログインしてないユーザーがマイページにアクセスした場合ログイン画面に遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
+  before_action :move_to_sign_in, only: [:show]
 
-  def index
-  end
 
   def show
     @user = current_user                      #ログイン中のユーザー情報の取得
@@ -10,5 +9,11 @@ class UsersController < ApplicationController
     @tweets_count = @tweets.count             #ログイン中ユーザーの総投稿数
     @chart = { "2020-01-01" => 100, "2020-01-02" => 80, "2020-01-03" => 110, "2020-01-04" => 130, "2020-01-05" => 90 }
 
+  end
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to "/users/sign_in"
+    end
   end
 end
