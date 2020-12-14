c394a45efa544bcf89266d12dc5a30885ce8f72c
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-12-14 10:12:47 +0900

    新規登録の結合テストの完成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
+    nickname              {"test11"}
     email                 {"test@example123"}
     password              {"tester123"}
     password_confirmation {password}
