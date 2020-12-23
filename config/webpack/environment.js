b0252ad85885e248e4716f3287952e5fbf212965
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-12-23 09:31:30 +0900

    jqueryの導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,15 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+// 以下を追記
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+// ここまで
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
